dead34089f476edee43dfa2f9cdcf978054c8878
maindconsultingservices <157203828+maindconsultingservices@users.noreply.github.com>
2024-09-29 11:46:29 +0200

    Delete utils/affiliate.ts

--- utils/affiliate.ts
+++
@@ -1,7 +0,0 @@
-import crypto from 'crypto';
-
-export function generateExternalAffiliateId(userId: number, programId: number, secretKey: string): string {
-  const data = `${userId}-${programId}`;
-  const hash = crypto.createHmac('sha256', secretKey).update(data).digest('hex');
-  return hash.substring(0, 12);
-}
