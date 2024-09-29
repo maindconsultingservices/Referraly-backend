8c95bc0c9008ed4a17acd3b3e1aa0c73344446f9
maindconsultingservices <157203828+maindconsultingservices@users.noreply.github.com>
2024-09-29 12:11:42 +0200

    Update [languageCode].ts

--- pages/api/ui-messages/[languageCode].ts
+++ pages/api/ui-messages/[languageCode].ts
@@ -1,12 +1,14 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import prisma from '../../../utils/db';
+import { sql } from '@vercel/postgres';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const languageCode = req.query.languageCode as string;
 
-  const messages = await prisma.uIMessage.findMany({
-    where: { languageCode },
-  });
+  const result = await sql`
+    SELECT message_key, message_text
+    FROM ui_messages
+    WHERE language_code = ${languageCode};
+  `;
 
-  res.status(200).json(messages);
+  res.status(200).json(result.rows);
 }
