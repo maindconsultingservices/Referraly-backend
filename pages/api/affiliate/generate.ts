243c5e55beef759307d65724a7146392eceabcf2
maindconsultingservices <157203828+maindconsultingservices@users.noreply.github.com>
2024-09-29 12:11:06 +0200

    Update generate.ts

--- pages/api/affiliate/generate.ts
+++ pages/api/affiliate/generate.ts
@@ -1,5 +1,5 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import prisma from '../../../utils/db';
+import { sql } from '@vercel/postgres';
 import { authMiddleware } from '../../../middleware/authMiddleware';
 import crypto from 'crypto';
 
@@ -8,14 +8,33 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { programId } = req.body;
 
   const secretKey = process.env.SECRET_KEY!;
-
   const data = `${userId}-${programId}`;
   const hash = crypto.createHmac('sha256', secretKey).update(data).digest('hex');
   const externalId = hash.substring(0, 12);
 
-  await prisma.externalAffiliateId.create({
-    data: { userId, programId, externalId },
-  });
+  // Check if the external affiliate ID already exists
+  const existingId = await sql`
+    SELECT id FROM external_affiliate_ids
+    WHERE user_id = ${userId} AND program_id = ${programId};
+  `;
+
+  if (existingId.rowCount === 0) {
+    await sql`
+      INSERT INTO external_affiliate_ids (
+        user_id,
+        program_id,
+        external_id,
+        created_at,
+        updated_at
+      ) VALUES (
+        ${userId},
+        ${programId},
+        ${externalId},
+        NOW(),
+        NOW()
+      );
+    `;
+  }
 
   res.status(201).json({ externalId });
 }
