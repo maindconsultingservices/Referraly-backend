0a528cde2a448a49b12e5f745edad7054caa1c7e
maindconsultingservices <157203828+maindconsultingservices@users.noreply.github.com>
2024-09-29 21:20:40 +0200

    Update ProfileScreen.kt

--- android-app/app/src/main/java/com/referally/ui/screens/ProfileScreen.kt
+++ android-app/app/src/main/java/com/referally/ui/screens/ProfileScreen.kt
@@ -1,11 +1,11 @@
-package com.example.appname.ui.screens
+package com.referally.ui.screens
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
-import com.example.appname.viewmodel.ProfileViewModel
+import com.referally.ui.screens.viewmodel.ProfileViewModel
 
 @Composable
 fun ProfileScreen(navController: NavHostController, viewModel: ProfileViewModel = viewModel()) {
