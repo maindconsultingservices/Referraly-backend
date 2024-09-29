dd1e9eff0ea7e2a4e633114ed3a4a6530ad1f483
maindconsultingservices <157203828+maindconsultingservices@users.noreply.github.com>
2024-09-29 21:17:44 +0200

    Update GDPRViewModel.kt

--- android-app/app/src/main/java/com/referally/ui/viewmodel/GDPRViewModel.kt
+++ android-app/app/src/main/java/com/referally/ui/viewmodel/GDPRViewModel.kt
@@ -1,8 +1,8 @@
-package com.referally.viewmodel
+package com.referally.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.referally.repository.GDPRRepository
+import com.referally.ui.repository.GDPRRepository
 import kotlinx.coroutines.launch
 
 class GDPRViewModel(private val repository: GDPRRepository = GDPRRepository()) : ViewModel() {
