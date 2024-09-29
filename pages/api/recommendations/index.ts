4def4f4e823213191fd4518eb8d456d44a12253e
maindconsultingservices <157203828+maindconsultingservices@users.noreply.github.com>
2024-09-29 12:10:03 +0200

    Update index.ts

--- pages/api/recommendations/index.ts
+++ pages/api/recommendations/index.ts
@@ -1,23 +1,35 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import prisma from '../../../utils/db';
+import { sql } from '@vercel/postgres';
 import { authMiddleware } from '../../../middleware/authMiddleware';
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   const userId = req.body.userId;
 
   if (req.method === 'GET') {
-    const recommendations = await prisma.recommendation.findMany({
-      where: { userId },
-    });
-    res.status(200).json(recommendations);
+    const result = await sql`SELECT * FROM recommendations WHERE user_id = ${userId};`;
+    res.status(200).json(result.rows);
   } else if (req.method === 'POST') {
     const { content, programId } = req.body;
 
-    const recommendation = await prisma.recommendation.create({
-      data: { content, programId, userId },
-    });
+    await sql`
+      INSERT INTO recommendations (
+        user_id,
+        content,
+        program_id,
+        created_at,
+        updated_at,
+        is_active
+      ) VALUES (
+        ${userId},
+        ${content},
+        ${programId},
+        NOW(),
+        NOW(),
+        TRUE
+      );
+    `;
 
-    res.status(201).json(recommendation);
+    res.status(201).json({ message: 'Recommendation created successfully' });
   } else {
     res.status(405).json({ error: 'Method not allowed' });
   }
