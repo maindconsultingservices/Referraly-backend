c4470115dce50f36c81832c6c963983968f2c30d
maindconsultingservices <157203828+maindconsultingservices@users.noreply.github.com>
2024-09-29 21:15:39 +0200

    Update Constants.kt

--- android-app/app/src/main/java/com/referally/ui/utils/Constants.kt
+++ android-app/app/src/main/java/com/referally/ui/utils/Constants.kt
@@ -1,5 +1,5 @@
-package com.referally.utils
+package com.referraly.ui.utils
 
 object Constants {
-    const val BASE_URL = "https://referally.vercel.app/"
+    const val BASE_URL = "https://referraly-app.vercel.app/"
 }
